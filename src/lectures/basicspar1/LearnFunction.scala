816039234787c4df5cd611e2527813c6f0713920
shubham mishra <shubhammishra@shubhams-MacBook-Pro.local>
2020-08-24 01:46:51 +0530

    Adding assigments

--- src/lectures/basicspar1/LearnFunction.scala
+++ src/lectures/basicspar1/LearnFunction.scala
@@ -23,5 +23,49 @@ object LearnFunction extends App{
   // but for recursive functions return type is must .
   // We can define return type as unit for function which gives side effects .
 
+  def bigfunction(x: Int) = {
+    def smallfunction()= x+2   // Auxiliary function declaration
+    smallfunction()
+  }
+  println(bigfunction(5))
+
+  // Assignments
+  //1.Greet Function - greetfunction(name,age)-"Hello my name is $name and my age $age"
+  //2.factorial
+  //3.Fibonacci
+  //isprimenumber
+
+
+  def greetfunction(name : String , age: Int) : String ={
+    "Hi my name is " + name + " and my age is "+age
+  }
+  println(greetfunction("shubham",27))
+
+  def factorial(x: Int) : Int={
+    if (x==1) x
+    else x*factorial(x-1)
+
+  }
+  println(factorial(4))
+
+  def fibbonacci(x: Int) : Int={
+    val a=1
+    if ((x==1) | (x==2) ) a
+    else fibbonacci(x-1) + fibbonacci(x-2)
+  }
+  println(fibbonacci(6))
+
+  def isprime(n: Int): Boolean={
+    def checkprime(t:Int):Boolean= {
+
+      if (t <= 1) true
+      else n%t!=0 && checkprime(t-1)
+
+    }
+    checkprime(n-1)
+
+  }
+  println(isprime(10))
+
 
 }
